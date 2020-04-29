036b017aa8f74d7a09d2534ff89393268cd53625
sabdesamie <64537096+sabdesamie@users.noreply.github.com>
2020-04-29 18:16:52 +0200

    Update balance_data.py

--- Tutorial Codes/Part 8-13 code/balance_data.py
+++ Tutorial Codes/Part 8-13 code/balance_data.py
@@ -1,45 +1,44 @@
 # balance_data.py
 
 import numpy as np
-import pandas as pd
-from collections import Counter
 from random import shuffle
 
-train_data = np.load('training_data.npy')
+train_data = []
 
-df = pd.DataFrame(train_data)
-print(df.head())
-print(Counter(df[1].apply(str)))
+for i in range(105):
+    
+    # Because of correpted files .
+    if (i != 5 and i != 6 and i != 8 and i!= 14 and i != 20 and i != 26 and i != 101):
+        train_data += list(np.load("watch_dogs_"+str(i)+"_training_data.npy",allow_pickle=True))
 
+forwards = []
 lefts = []
 rights = []
-forwards = []
-
-shuffle(train_data)
 
 for data in train_data:
-    img = data[0]
-    choice = data[1]
 
-    if choice == [1,0,0]:
-        lefts.append([img,choice])
-    elif choice == [0,1,0]:
-        forwards.append([img,choice])
-    elif choice == [0,0,1]:
-        rights.append([img,choice])
-    else:
-        print('no matches')
+    screen = data[0]
+    move = data[1]
 
+    if move == [1,0,0]:
+        lefts.append([screen,move])
+    elif move == [0,1,0]:
+        forwards.append([screen,move])
+    elif move == [0,0,1]:
+        rights.append([screen,move])
 
-forwards = forwards[:len(lefts)][:len(rights)]
-lefts = lefts[:len(forwards)]
-rights = rights[:len(forwards)]
+SIZE = min(len(lefts),len(rights),len(forwards))
 
-final_data = forwards + lefts + rights
-shuffle(final_data)
+lefts = lefts[:SIZE]
+rights = rights[:SIZE]
+forwards = forwards[:SIZE]
 
-np.save('training_data.npy', final_data)
+train_data = lefts + rights + forwards
 
+shuffle(train_data)
+print(len(train_data))
+
+np.save('watch_dogs_total_balanced_data.npy', train_data)
 
 
 
