f733ed7f725c50d2e7766e110712a07a82ef9ac6
sabdesamie <64537096+sabdesamie@users.noreply.github.com>
2020-04-29 18:01:58 +0200

    Update create_training_data.py

--- Tutorial Codes/Part 8-13 code/create_training_data.py
+++ Tutorial Codes/Part 8-13 code/create_training_data.py
@@ -1,75 +1,47 @@
 # create_training_data.py
 
 import numpy as np
-from grabscreen import grab_screen
 import cv2
+from PIL import ImageGrab
 import time
-from getkeys import key_check
+from get_keys import key_check
 import os
-
+from grabscreen import grab_screen
 
 def keys_to_output(keys):
-    '''
-    Convert keys to a ...multi-hot... array
-
-    [A,W,D] boolean values.
-    '''
     output = [0,0,0]
-    
-    if 'A' in keys:
+    if "Q" in keys:
         output[0] = 1
-    elif 'D' in keys:
+    elif "D" in keys:
         output[2] = 1
     else:
         output[1] = 1
     return output
 
+for i in list(range(4)):
+    print(4-i)
+    time.sleep(1)
 
-file_name = 'training_data.npy'
+file_name = "watch_dogs_119_training_data.npy"
 
 if os.path.isfile(file_name):
-    print('File exists, loading previous data!')
-    training_data = list(np.load(file_name))
+    print("file exist")
+    training_data = list(np.load(file_name,allow_pickle=True))
 else:
-    print('File does not exist, starting fresh!')
+    print("file does not exist")
     training_data = []
 
 
-def main():
 
-    for i in list(range(4))[::-1]:
-        print(i+1)
-        time.sleep(1)
-
-
-    paused = False
-    while(True):
-
-        if not paused:
-            # 800x600 windowed mode
-            screen = grab_screen(region=(0,40,800,640))
-            last_time = time.time()
-            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-            screen = cv2.resize(screen, (160,120))
-            # resize to something a bit more acceptable for a CNN
-            keys = key_check()
-            output = keys_to_output(keys)
-            training_data.append([screen,output])
-            
-            if len(training_data) % 1000 == 0:
-                print(len(training_data))
-                np.save(file_name,training_data)
-
-        keys = key_check()
-        if 'T' in keys:
-            if paused:
-                paused = False
-                print('unpaused!')
-                time.sleep(1)
-            else:
-                print('Pausing!')
-                paused = True
-                time.sleep(1)
+while(True):
+    screen = grab_screen(region = (0,40,800,640))
+    screen = cv2.cvtColor(screen,cv2.COLOR_BGR2GRAY)
+    screen = cv2.resize(screen,(80,60))
+    keys = key_check()
+    output = keys_to_output(keys)
+    training_data.append([screen,output])
 
 
-main()
+    if len(training_data) % 1000 == 0:
+        print(len(training_data))
+        np.save(file_name,training_data)
