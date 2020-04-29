19dd98329f613771a5a78b354557ff81199e66b5
sabdesamie <64537096+sabdesamie@users.noreply.github.com>
2020-04-29 18:52:40 +0200

    Update train_model.py

--- Tutorial Codes/Part 8-13 code/train_model.py
+++ Tutorial Codes/Part 8-13 code/train_model.py
@@ -1,22 +1,33 @@
 # train_model.py
 
 import numpy as np
+import cv2
+from PIL import ImageGrab
+import time
+from get_keys import key_check
+import os
+from grabscreen import grab_screen
 from alexnet import alexnet
-WIDTH = 160
-HEIGHT = 120
+from directkey import PressKey, ReleaseKey,A,W,D
+
+
+WIDTH = 80
+HEIGHT = 60
 LR = 1e-3
-EPOCHS = 10
-MODEL_NAME = 'pygta5-car-fast-{}-{}-{}-epochs-300K-data.model'.format(LR, 'alexnetv2',EPOCHS)
+EPOCHS = 4
+MODEL_NAME = "pywd_model_v3_final.h5"
 
-model = alexnet(WIDTH, HEIGHT, LR)
+model = alexnet(WIDTH,HEIGHT,LR)
+model.load(MODEL_NAME)
 
-hm_data = 22
+
+train_data = np.load('watch_dogs_total_balanced_data.npy',allow_pickle=True)
+train = train_data[:-100]
+test = train_data[-100:]
+
+
 for i in range(EPOCHS):
-    for i in range(1,hm_data+1):
-        train_data = np.load('training_data-{}-balanced.npy'.format(i))
 
-        train = train_data[:-100]
-        test = train_data[-100:]
 
         X = np.array([i[0] for i in train]).reshape(-1,WIDTH,HEIGHT,1)
         Y = [i[1] for i in train]
@@ -24,11 +35,10 @@ for i in range(EPOCHS):
         test_x = np.array([i[0] for i in test]).reshape(-1,WIDTH,HEIGHT,1)
         test_y = [i[1] for i in test]
 
-        model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}), 
+        model.fit({'input': X}, {'targets': Y}, n_epoch=1, validation_set=({'input': test_x}, {'targets': test_y}),
             snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
 
-        model.save(MODEL_NAME)
-
+        model.save("wd_model_v3.h5")
 
 
 # tensorboard --logdir=foo:C:/path/to/log
